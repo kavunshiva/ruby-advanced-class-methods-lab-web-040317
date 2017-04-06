6cdf1ccb614938101bdb4c1651a378785693884d
Isaac Anderson <isaac.s.anderson@gmail.com>
2017-04-06 21:26:02 +0000

    finish song in an operatic tour de force

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.each do |song|
+      return song if song.name == name
+    end
+    nil
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name) != nil
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by do |song|
+      song.name
+    end
+  end
+
+  def self.new_from_filename(filename)
+    song = self.new_by_name(filename.split("-").last[1..-1].split(".").first)
+    song.artist_name = filename.split("-").first[0...-1]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
